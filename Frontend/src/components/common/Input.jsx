565e108aaa05ee630648bd20c7e97d2a6147355a
Mauricio Ramirez <maurizio_514@hotmail.com>
2026-06-20 15:22:34 -0500

    Conexiones Frontend-backend

--- Frontend/src/components/common/Input.jsx
+++ Frontend/src/components/common/Input.jsx
@@ -1,36 +1,16 @@
-function Input({
-label,
-type="text",
-placeholder
-}){
-
+function Input({ label, type="text", value, onChange, placeholder }){
 
 return(
 
-<div className="flex flex-col gap-2">
-
-
-<label className="font-medium">
-{label}
-</label>
-
+<div>
+<label>{label}</label>
 
 <input
-
 type={type}
-
+value={value}
+onChange={onChange}
 placeholder={placeholder}
-
-className="
-border
-rounded-lg
-px-4
-py-2
-focus:outline-none
-focus:ring-2
-focus:ring-blue-500
-"
-
+className="w-full border px-4 py-2 rounded-lg"
 />
 
 </div>
@@ -38,6 +18,5 @@ focus:ring-blue-500
 )
 
 }
-
 
 export default Input;
--- Frontend/src/pages/public/Register.jsx
+++ Frontend/src/pages/public/Register.jsx
@@ -1,55 +1,73 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Input from "../../components/common/Input";
 import Button from "../../components/common/Button";
+
+import { registerRequest } from "../../services/authService";
 
 
 function Register(){
 
+const navigate = useNavigate();
+
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+
+const handleSubmit = async (e) => {
+e.preventDefault();
+console.log(name, email, password);
+
+  try {
+console.log(name, email, password);
+    await registerRequest({
+      name,
+      email,
+      password
+    });
+
+    alert("Usuario creado");
+    navigate("/login");
+
+  } catch (error) {
+    alert(error.response?.data?.message || "Error en registro");
+  }
+};
+
+
 return(
 
-<div className="
-min-h-screen
-flex
-items-center
-justify-center
-bg-gray-100
-">
+<div className="min-h-screen flex items-center justify-center bg-gray-100">
 
-<div className="
-bg-white
-p-8
-rounded-2xl
-shadow-lg
-w-full
-max-w-md
-">
+<div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
 
-
-<h2 className="
-text-2xl
-font-bold
-text-center
-text-primary
-mb-6
-">
+<h2 className="text-2xl font-bold text-center text-primary mb-6">
 Crear cuenta
 </h2>
 
-
-<form className="space-y-4">
+<form onSubmit={handleSubmit} className="space-y-4">
 
 <Input
 label="Nombre"
+value={name}
+onChange={(e)=>setName(e.target.value)}
 placeholder="Tu nombre"
 />
 
 <Input
 label="Correo electrónico"
 type="email"
+value={email}
+onChange={(e)=>setEmail(e.target.value)}
 />
 
 <Input
 label="Contraseña"
 type="password"
+value={password}
+onChange={(e)=>setPassword(e.target.value)}
 />
 
 <Button type="submit">
@@ -58,19 +76,14 @@ Registrarse
 
 </form>
 
-
 <p className="text-sm text-center mt-4">
-
 ¿Ya tienes cuenta?  
 <a href="/login" className="text-primary font-semibold">
  Inicia sesión
 </a>
-
 </p>
 
-
 </div>
-
 </div>
 
 )
