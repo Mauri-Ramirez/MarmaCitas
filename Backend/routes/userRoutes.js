53f4eb67aed8ca3ded5d1fe62f0b6d8571775aaa
Mauricio Ramirez <maurizio_514@hotmail.com>
2026-06-25 02:12:02 -0500

    implementar autenticación con JWT y MongoDB

---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import verifyToken from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/profile", verifyToken, (req, res) => {
+  res.json({
+    message: "Acceso permitido",
+    user: req.user,
+  });
+});
+
+export default router;
